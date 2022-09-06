0ba65fde0092641c4b80a9e4f767e9ca2af0a700
nicolaubarnekow <mateustester@gmail.com>
2022-09-06 17:45:03 -0300

    Aula 12 sobre condicionais I

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,28 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Qual é a sua nacionalidade?');
+
+const nacionalidadeConst = nacionalidade
+
+if (nacionalidadeConst === "brasileira"){ 
+    alert('Você é brasileiro! Login Aceito')
+    console.log("Brasileiro")
+    }
+ else if (nacionalidadeConst === "uruguaia"){
+    alert('Você é uruguaio! Login aceito')
+    console.log("Uruguaio")
+ } 
+ else if (nacionalidadeConst === "argentina"){
+    alert('Você é argentino! login aceito')
+    console.log("Argentino")
+ }
+ else if (nacionalidadeConst === " Chilena "){
+    alert('Você é Chileno! Login aceito')
+    console.log('Chileno')
+ }
+else if (nacionalidadeConst === " colombiano "){
+    alert(' Você é Colombiano! Login aceito')
+    console.log("Colombiano")
+}
+else {
+    alert('Como não respondeu o que foi pedido, imagino que deve ser burro. Por favor, tente novamente')
+    console.log('Nacionalidade não encontrada')
+}
